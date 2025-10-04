9964e12e87b163bf62f89fdd73551f9ff5cc58d5
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-04 07:38:38 -0300

    routed successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,8 @@
 import { Toaster } from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 
 function App() {
@@ -7,10 +11,17 @@ function App() {
   return (
     <>
       <Toaster position="top-right" reverseOrder={false} />
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold">Welcome to Portiflex</h1>
-        <p className="mt-4 text-red-50">Your one-stop solution for all your portfolio needs.</p>
-      </div>
+      {/* <AuthProvider> */}
+        <div className="min-h-screen bg-gray-50 flex flex-col">
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      {/* </AuthProvider> */}
     </>
   )
 }
