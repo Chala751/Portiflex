3de31205a3d6345c37d7aa652316d1acf8c640e5
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-04 07:22:55 -0300

    toaster rapped

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Toaster } from "react-hot-toast";
 
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
 
   return (
     <>
+      <Toaster position="top-right" reverseOrder={false} />
       <div className="container mx-auto">
         <h1 className="text-4xl font-bold">Welcome to Portiflex</h1>
         <p className="mt-4 text-red-50">Your one-stop solution for all your portfolio needs.</p>
