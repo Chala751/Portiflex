4804b76fe39cdf31a04638e766cad6829a9ad53c
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-05 07:29:54 -0300

    db connected success

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./configs/db.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 app.use(express.json());
