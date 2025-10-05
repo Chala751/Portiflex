c53880e1929212b2d7e65ae21a98b7c6e3195a79
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-05 06:45:13 -0300

    server runing done

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend origin
+    credentials: true, // Allow credentials
+  })
+);
+
+// Routes
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process?.env?.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
