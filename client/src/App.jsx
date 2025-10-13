f404257882e8bae0f676725bc7fe7569d5fce162
Chala Temesgen <ctemesgen85@gmail.com>
2025-10-13 14:27:28 -0300

    pages routed success

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 
 function App() {
@@ -16,6 +19,9 @@ function App() {
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/projects" element={<Project/>} />
+              <Route path="/contact" element={<Contact/>} />
             </Routes>
           </main>
           <Footer />
